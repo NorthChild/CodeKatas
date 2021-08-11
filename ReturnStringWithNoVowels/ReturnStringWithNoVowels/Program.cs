54337748a3c4cdc8947132bb3c8a690c45b2e836
michael carini <snownkc@icloud.com>
2021-08-11 18:27:24 +0100

    created folder

--- ReturnStringWithNoVowels/ReturnStringWithNoVowels/Program.cs
+++ ReturnStringWithNoVowels/ReturnStringWithNoVowels/Program.cs
@@ -1,13 +1,33 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace ReturnStringWithNoVowels
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             // write a method, takes a string and remove the vowels and return the string without the vowels 
+
+            // a e i o u 
+
+            var testString = " ";
+
+
         }
+
+
+        public static string Disemvowelling(string testString) 
+        {
+            
+
+            return "";
+        }
+
+
+
+
     }
 }
