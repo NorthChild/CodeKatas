b9a8896b36b750d75d8b9bdf13ee3ee2bbc5c593
michael carini <snownkc@icloud.com>
2021-08-11 18:27:51 +0100

    set up program

---
+++ DuplicateElemCounter/DuplicateElemCounter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DuplicateElemCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Create a method which takes a string and returns an array of duplicate characters e.g. "Nishant Mandal" => (a,n)
+
+
+
+        }
+    }
+}
---
+++ DuplicateElemCounter/DuplicateElementTEST/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using DuplicateElementTEST;
+
+namespace DuplicateElementTEST
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
