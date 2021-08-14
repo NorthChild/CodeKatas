c612b08813a8e0dda67fb7e30892d10c4fdef29d
michael carini <snownkc@icloud.com>
2021-08-14 17:03:06 +0100

    finished

--- DuplicateElemCounter/DuplicateElemCounter/Program.cs
+++ DuplicateElemCounter/DuplicateElemCounter/Program.cs
@@ -1,16 +1,63 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace DuplicateElemCounter
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             // Create a method which takes a string and returns an array of duplicate characters e.g. "Nishant Mandal" => (a,n)
 
+            //string testString = "Nishant Mandal";
+            string testString = "Nanananananananana Batman";
+            //string testString = "";
 
+
+            Console.WriteLine(DupicateCounter(testString));
 
         }
+
+
+        public static char[] DupicateCounter(string testString)
+        {
+
+            if (testString.Length <= 0) 
+            {
+                throw new ArgumentOutOfRangeException("Give me something to do!");
+            }
+
+            var testStr = testString.ToLower();
+            string recurringCharString = "";
+            string dumpString = "";
+
+            List<char> recurringCharactersList = new List<char>();
+
+            foreach ( var singleChar in testStr)
+            {
+                if (!recurringCharactersList.Contains(singleChar))
+                {
+                    recurringCharactersList.Add(singleChar);
+                }
+                else
+                {
+                    if (!recurringCharString.Contains(singleChar) && singleChar != ' ')
+                    {
+                        recurringCharString += singleChar;
+                    }
+                    else
+                    {
+                        dumpString += singleChar;
+                    }
+                }
+            }
+
+
+            char[] ResultArray = recurringCharString.ToArray();
+            return ResultArray;
+        }
+
+
     }
 }
--- DuplicateElemCounter/DuplicateElementTEST/UnitTest1.cs
+++ DuplicateElemCounter/DuplicateElementTEST/UnitTest1.cs
@@ -1,5 +1,8 @@
 using NUnit.Framework;
-using DuplicateElementTEST;
+using DuplicateElemCounter;
+using System;
+using System.Collections.Generic;
+
 
 namespace DuplicateElementTEST
 {
@@ -10,10 +13,23 @@ namespace DuplicateElementTEST
         {
         }
 
-        [Test]
-        public void Test1()
+        [TestCase("anna mia", new char[]{'n','a'})]
+        [TestCase("Nishant Mandal", new char[] {'n','a'})]
+        [TestCase("whatever tommorow brings i'll be there", new char[] { 'e', 't', 'm', 'o', 'r', 'w', 'i', 'l', 'b', 'h' })]
+        [TestCase("test string, not too deep", new char[] { 't', 's', 'n', 'o', 'e' })]
+        public void FeedingStringsReturningACountOfDuplicateCharacters(string testString, char[] expected)
         {
-            Assert.Pass();
+            Assert.That(() => Program.DupicateCounter(testString), Is.EqualTo(expected));
         }
+
+
+        [Test]
+        public void FeedingEmptyStringReturnsException()
+        {
+            var emptyTestString = "";
+            Assert.That(() => Program.DupicateCounter(emptyTestString), Throws.TypeOf<ArgumentOutOfRangeException>().With.Message.Contains("Give me something to do!"));
+        }
+
+
     }
 }
