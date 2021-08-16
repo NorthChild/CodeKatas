cf6aea1c842cc4cdc4f31346e0ba833c243c0309
michael carini <snownkc@icloud.com>
2021-08-16 19:48:19 +0100

    exercise set up done

---
+++ ReturnFibonacciSum/ReturnFibonacciSum/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ReturnFibonacciSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Create a method which returns the sum of fibonacci numbers up to the nth fibonacci number, nth number included
+        }
+    }
+}
