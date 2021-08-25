abb12e8454572138ff704b6c63e03560d8c1c770
michael carini <snownkc@icloud.com>
2021-08-25 12:34:50 +0100

    added ulterior method of solving 'reverse string' exercise

--- TakeAStringAndReverseIt/TakeAStringAndReverseIt/Program.cs
+++ TakeAStringAndReverseIt/TakeAStringAndReverseIt/Program.cs
@@ -12,12 +12,19 @@ namespace TakeAStringAndReverseIt
 
             Console.WriteLine("Insert a string to reverse: ");
             var testString = Console.ReadLine();
+            Console.WriteLine();
 
-            Console.WriteLine(TakeStringAndReverseIt(testString));
+            // method 1: using stack
+            Console.WriteLine($"Using stack: {TakeStringAndReverseIt(testString)}");
+            // method 2: using for loop
+            Console.WriteLine($"Using for loop: {TakeAStringAndReverseItOtherMethod(testString)}");
         }
 
         public static string TakeStringAndReverseIt(string testString)
         {
+
+            if (testString.Length == 0) throw new ArgumentOutOfRangeException("string is empty");
+            if (testString.Length == 1) return testString;
 
             Stack reversedStringStack = new Stack();
             string stringResult = "";
@@ -30,8 +37,23 @@ namespace TakeAStringAndReverseIt
             {
                 stringResult += i;
             }
-
             return stringResult;
+        }
+
+        public static string TakeAStringAndReverseItOtherMethod(string testString)
+        {
+
+            if (testString.Length == 0) throw new ArgumentOutOfRangeException("string is empty");
+            if (testString.Length == 1) return testString;
+
+            int stringLen = testString.Length - 1;
+            var resultString = "";
+
+            for (int i = testString.Length - 1; i >= 0; i--)
+            {
+                resultString += testString[i];
+            }
+            return resultString;
         }
     }
 }
--- TakeAStringAndReverseIt/TestingReversedString/UnitTest1.cs
+++ TakeAStringAndReverseIt/TestingReversedString/UnitTest1.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using TakeAStringAndReverseIt;
+using System;
 
 namespace TestingReversedString
 {
@@ -12,9 +13,34 @@ namespace TestingReversedString
 
         [TestCase("This is my string", "gnirts ym si sihT")]
         [TestCase("This is another string", "gnirts rehtona si sihT")]
+        [TestCase("a", "a")]
         public void TestingThatReceivedStringIsReversedCorrectly(string testString, string expected)
         {
-            Assert.That(()=> Program.TakeStringAndReverseIt(testString), Is.EqualTo(expected));
+            Assert.That(() => Program.TakeStringAndReverseIt(testString), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void TestingThatFeedingEmptyStringThrowsException()
+        {
+            var emptyTestString = "";
+            Assert.That(() => Program.TakeStringAndReverseIt(emptyTestString), Throws.TypeOf<ArgumentOutOfRangeException>().With.Message.Contains("string is empty"));
+        }
+
+
+        // other method 
+        [TestCase("This is my string", "gnirts ym si sihT")]
+        [TestCase("This is another string", "gnirts rehtona si sihT")]
+        [TestCase("a", "a")]
+        public void TestingThatReceivedStringIsReversedCorrectlyOtherMethod(string testString, string expected)
+        {
+            Assert.That(() => Program.TakeAStringAndReverseItOtherMethod(testString), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void TestingThatFeedingEmptyStringThrowsExceptionOtherMethod()
+        {
+            var emptyTestString = "";
+            Assert.That(() => Program.TakeAStringAndReverseItOtherMethod(emptyTestString), Throws.TypeOf<ArgumentOutOfRangeException>().With.Message.Contains("string is empty"));
         }
     }
 }
