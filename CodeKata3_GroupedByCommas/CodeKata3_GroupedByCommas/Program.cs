9751b362fa9fb32ce710aed8f65a3d14e1567032
michael carini <snownkc@icloud.com>
2021-09-19 16:26:35 +0100

    setup of code kata3 done

---
+++ CodeKata3_GroupedByCommas/CodeKata3_GroupedByCommas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CodeKata3_GroupedByCommas
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var testInt = 123;
+
+            Console.WriteLine(GroupByMethod(testInt));
+
+        }
+
+        public static string GroupByMethod(int n)
+        {
+            var result = ReturnIntGroupedWithCommas(n);
+            return result;
+        }
+
+
+        public static string ReturnIntGroupedWithCommas(int n)
+        {
+            return "ready";
+        }
+
+
+    }
+}
---
+++ CodeKata3_GroupedByCommas/TestingCodeKata3Solution/Happy Paths/GivenAnIntReturnRightGorupingByCommas.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CodeKata3_GroupedByCommas;
+
+namespace TestingCodeKata3Solution.Happy_Paths
+{
+    public class GivenAnIntReturnRightGorupingByCommas
+    {
+        [TestFixture]
+        public class SolutionTest
+        {
+            [Test]
+            public void GetPositiveIntegersAndReturnGroupedByComma()
+            {
+                Assert.That(Program.GroupByMethod(1), Is.EqualTo("1"));
+                Assert.That(Program.GroupByMethod(12), Is.EqualTo("12"));
+                Assert.That(Program.GroupByMethod(123), Is.EqualTo("123"));
+                Assert.That(Program.GroupByMethod(1234), Is.EqualTo("1,234"));
+                Assert.That(Program.GroupByMethod(12345), Is.EqualTo("12,345"));
+                Assert.That(Program.GroupByMethod(123456), Is.EqualTo("123,456"));
+                Assert.That(Program.GroupByMethod(1234567), Is.EqualTo("1,234,567"));
+                Assert.That(Program.GroupByMethod(12345678), Is.EqualTo("12,345,678"));
+                Assert.That(Program.GroupByMethod(123456789), Is.EqualTo("123,456,789"));
+                Assert.That(Program.GroupByMethod(1234567890), Is.EqualTo("1,234,567,890"));
+            }
+
+
+            [Test]
+            public void GetNegativeIntegersAndReturnGroupedByComma()
+            {
+                Assert.That(Program.GroupByMethod(-1), Is.EqualTo("-1"));
+                Assert.That(Program.GroupByMethod(-12), Is.EqualTo("-12"));
+                Assert.That(Program.GroupByMethod(-123), Is.EqualTo("-123"));
+                Assert.That(Program.GroupByMethod(-1234), Is.EqualTo("-1,234"));
+                Assert.That(Program.GroupByMethod(-12345), Is.EqualTo("-12,345"));
+                Assert.That(Program.GroupByMethod(-123456), Is.EqualTo("-123,456"));
+                Assert.That(Program.GroupByMethod(-1234567), Is.EqualTo("-1,234,567"));
+                Assert.That(Program.GroupByMethod(-12345678), Is.EqualTo("-12,345,678"));
+                Assert.That(Program.GroupByMethod(-123456789), Is.EqualTo("-123,456,789"));
+                Assert.That(Program.GroupByMethod(-1234567890), Is.EqualTo("-1,234,567,890"));
+            }
+        }
+    }
+}
