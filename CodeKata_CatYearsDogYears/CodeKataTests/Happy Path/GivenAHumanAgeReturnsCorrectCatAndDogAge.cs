713fb0a29317c1cb1fa17de055c9b0628395cd24
michael carini <snownkc@icloud.com>
2021-09-19 18:04:37 +0100

    code kata 2 setup complete, ready to be attempted

---
+++ CodeKata_CatYearsDogYears/CodeKataTests/Happy Path/GivenAHumanAgeReturnsCorrectCatAndDogAge.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CodeKata_CatYearsDogYears;
+
+namespace CodeKataTests.Happy_Path
+{
+    class GivenAHumanAgeReturnsCorrectCatAndDogAge
+    {
+        [Test]
+        public void One()
+        {
+            Assert.AreEqual(new int[] { 1, 15, 15 }, Program.GivenHumanAgeReturnCatDogAge(1));
+        }
+
+        [Test]
+        public void Two()
+        {
+            Assert.AreEqual(new int[] { 2, 24, 24 }, Program.GivenHumanAgeReturnCatDogAge(2));
+        }
+
+        [Test]
+        public void Ten()
+        {
+            Assert.AreEqual(new int[] { 10, 56, 64 }, Program.GivenHumanAgeReturnCatDogAge(10));
+        }
+    }
+}
---
+++ CodeKata_CatYearsDogYears/CodeKata_CatYearsDogYears/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CodeKata_CatYearsDogYears
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var testIntAge = 1;
+            GivenHumanAgeReturnCatDogAge(testIntAge);
+
+
+        }
+
+        public static int[] GivenHumanAgeReturnCatDogAge(int humanAge)
+        {
+            int[] intArrayTEst = new int[] { };
+            return intArrayTEst;
+            
+        }
+
+
+
+
+    }
+}
