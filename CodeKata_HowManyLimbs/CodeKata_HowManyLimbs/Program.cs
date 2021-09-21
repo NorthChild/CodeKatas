51daa0461ab6b11c3b9e4a521f7f6f2e63568886
michael carini <snownkc@icloud.com>
2021-09-21 09:23:07 +0100

    added completed code Kata, IMultiLimbs

---
+++ CodeKata_HowManyLimbs/CodeKata_HowManyLimbs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeKata_HowManyLimbs
+{
+
+    public interface IMultiLimb
+    {
+        public string Name { get; set; }
+        public int NumberOfLimbs { get; set; }
+    }
+    public class Animal : IMultiLimb
+    {
+        public string Name { get; set; }
+        public int NumberOfLimbs { get; set; }
+    }
+    public class Alien : IMultiLimb
+    {
+        public string Name { get; set; }
+        public int NumberOfLimbs { get; set; }
+    }
+
+    public class SortedByLimbs
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static List<IMultiLimb> Sort<T>(List<IMultiLimb> list) where T : IMultiLimb
+        {
+            return list.OrderByDescending(o => o.NumberOfLimbs).ToList();
+        }
+    }
+}
